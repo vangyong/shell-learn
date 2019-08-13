ebeca7632ec6baa0b5b7461f2032db42c68c37a8
wangyong <vangyong@126.com>
2019-08-13 09:25:05 +0800

    修改

--- grammar/test.sh
+++ grammar/test.sh
@@ -4,3 +4,16 @@ echo "What is your name?"
 read PERSON
 echo "Hello, $PERSON"
 
+var_name="变量1"
+echo $var_name
+echo ${#var_name}
+echo ${var_name:1:2}
+echo `expr index "$var_name" 量`
+
+for skill in Ada Coffe Action Java; do
+   echo "I am good at ${skill}Script"
+done
+
+array_name=(value0 value1 value2 value3)
+length = ${#array_name[@]}
+echo $length
