0bef92909999fecf1ee6993b275cca169a181ff4
wangyong <vangyong@126.com>
2020-01-03 11:01:48 +0800

    修改

---
+++ grammar/system_demo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "系统内置调用"
+echo ${BASH_SOURCE}
+echo ${BASH_SOURCE-$0}
---
+++ scripts/springboot_start_test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+JAVA_HOME=/opt/local/jdk
+JAVA_EXEC=$JAVA_HOME/bin/java
+APPLICATION_HOME=/opt/test
+
+
+# JVM Memory Control
+JAVA_OPTS="$JAVA_OPTS -XX:MaxMetaspaceSize=128m -Xmx1024M -XX:MinHeapFreeRatio=30 -XX:MaxHeapFreeRatio=50"
+# JAVA_OPTS="$JAVA_OPTS -XX:OnOutOfMemoryError=$KILL_SELF"
+
+# JMX config
+JMX_HOST=10.10.143.150
+JMX_PORT=18080
+JAVA_OPTS="$JAVA_OPTS \
+-Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=$JMX_HOST \
+-Dcom.sun.management.jmxremote.port=$JMX_PORT \
+-Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT \
+-Dcom.sun.management.jmxremote.local.only=false \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.ssl=false"
+
+# REMOTE DEBUG config
+# REMOTE_DEBUG_PORT=12345
+# JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,address=$REMOTE_DEBUG_PORT,server=y,suspend=n"
+
+
+# EXECUTE process
+exec $JAVA_EXEC $JAVA_OPTS -jar $APPLICATION_HOME/springboot1-test.jar --spring.config.location=file:$APPLICATION_HOME/application.yml
+
